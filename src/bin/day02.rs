1c5bcf67885ef295feb247fda569071e02430e7a
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2025-12-03 04:39:07 +0100

    day02 invalid id check

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -3,18 +3,39 @@ use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
 
+type Id = usize;
+
 fn main() -> anyhow::Result<()> {
     let mut s = String::new();
     File::open("input/input02.txt".to_string())?.read_to_string(&mut s)?;
 
-    println!("part 1: {}", todo!());
-    println!("part 2: {}", todo!());
+    // println!("part 1: {}", todo!());
+    // println!("part 2: {}", todo!());
 
     Ok(())
 }
 
+fn split_num(num: Id) -> Option<(Id, Id)> {
+    let num_digits = num.ilog10() + 1;
+    if !num_digits.is_multiple_of(2) {
+        return None;
+    }
+
+    let left_half = num / 10usize.pow(num_digits / 2);
+    let right_half = num % 10usize.pow(num_digits / 2);
+
+    Some((left_half, right_half))
+}
+
+fn is_id_invalid(num: Id) -> bool {
+    match split_num(num) {
+        Some((left_half, right_half)) => left_half == right_half,
+        None => false,
+    }
+}
+
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
-struct IdRange(usize, usize);
+struct IdRange(Id, Id);
 
 impl FromStr for IdRange {
     type Err = anyhow::Error;
@@ -29,9 +50,9 @@ impl FromStr for IdRange {
 mod test {
     use super::*;
 
-    const EXAMPLE: &'static str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-824824821-824824827,2121212118-2121212124";
+    //     const EXAMPLE: &'static str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+    // 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+    // 824824821-824824827,2121212118-2121212124";
 
     #[test]
     fn parse_ids() {
@@ -80,7 +101,21 @@ mod test {
     }
 
     #[test]
-    fn example1() {
-        
+    fn test_invalid_digits() {
+        dbg!(split_num(11));
+        dbg!(split_num(12));
+        dbg!(split_num(12));
+        dbg!(split_num(123));
+        dbg!(split_num(1188511885));
+        dbg!(split_num(1188511886));
+        assert!(is_id_invalid(11));
+        assert!(!is_id_invalid(12));
+        assert!(!is_id_invalid(12));
+        assert!(!is_id_invalid(123));
+        assert!(is_id_invalid(1188511885));
+        assert!(!is_id_invalid(1188511886));
     }
+
+    #[test]
+    fn example1() {}
 }
