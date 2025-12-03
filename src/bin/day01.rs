05631328655c5b07b321f7c169a65fa41fe06778
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2025-12-03 03:07:57 +0100

    day 01 part 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,6 +1,29 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
 use std::str::FromStr;
 
-fn main() {}
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut s = String::new();
+    File::open("input/input01.txt".to_string())?.read_to_string(&mut s)?;
+
+    println!("part 1: {}", process1(&s)?);
+
+    Ok(())
+}
+
+fn process1(s: &str) -> Result<u32, Box<dyn Error>> {
+    let mut dial = Dial::new(50, 100);
+    let mut count_zeros = 0;
+    for line in s.lines() {
+        let turn = Turn::from_str(line)?;
+        dial.turn_by(turn);
+        if dial.value == 0 {
+            count_zeros += 1;
+        };
+    }
+    Ok(count_zeros)
+}
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 enum Direction {
@@ -28,6 +51,27 @@ impl FromStr for Turn {
             .map_err(|e| format!("{e}"))?;
 
         Ok(Turn(dir, amount))
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Dial {
+    value: isize,
+    upper_bound: usize,
+}
+
+impl Dial {
+    fn new(value: isize, upper_bound: usize) -> Self {
+        Self { value, upper_bound }
+    }
+
+    fn turn_by(&mut self, turn: Turn) {
+        let sign = match turn.0 {
+            Direction::Left => -1,
+            Direction::Right => 1,
+        };
+
+        self.value = (self.value + sign * turn.1 as isize).rem_euclid(self.upper_bound as isize);
     }
 }
 
@@ -63,5 +107,66 @@ L82";
 
         assert!(Turn::from_str("Z10").is_err());
         assert!(Turn::from_str("L-10").is_err());
+    }
+
+    #[test]
+    fn test_dial() {
+        // normal
+        let mut dial = Dial::new(50, 100);
+        dial.turn_by(Turn(Direction::Left, 1));
+        assert_eq!(dial.value, 49);
+
+        let mut dial = Dial::new(50, 100);
+        dial.turn_by(Turn(Direction::Right, 1));
+        assert_eq!(dial.value, 51);
+
+        // overflow
+
+        let mut dial = Dial::new(50, 100);
+        dial.turn_by(Turn(Direction::Left, 101));
+        assert_eq!(dial.value, 49);
+
+        let mut dial = Dial::new(50, 100);
+        dial.turn_by(Turn(Direction::Right, 101));
+        assert_eq!(dial.value, 51);
+
+        // edge case
+
+        let mut dial = Dial::new(99, 100);
+        dial.turn_by(Turn(Direction::Right, 1));
+        assert_eq!(dial.value, 0);
+
+        let mut dial = Dial::new(0, 100);
+        dial.turn_by(Turn(Direction::Left, 0));
+        assert_eq!(dial.value, 0);
+    }
+
+    #[test]
+    fn example1() {
+        let mut dial = Dial::new(50, 100);
+        assert_eq!(dial.value, 50);
+
+        let mut turns = EXAMPLE.lines().map(|l| Turn::from_str(l).unwrap());
+
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 82);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 52);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 0);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 95);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 55);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 0);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 99);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 0);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 14);
+        dial.turn_by(turns.next().unwrap());
+        assert_eq!(dial.value, 32);
     }
 }
