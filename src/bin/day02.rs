5ce15c466b65837e5380cab849ea29ae62ad1349
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2025-12-03 04:02:09 +0100

    day02 framework

---
+++ src/bin/day02.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() -> anyhow::Result<()> {
+    let mut s = String::new();
+    File::open("input/input02.txt".to_string())?.read_to_string(&mut s)?;
+
+    println!("part 1: {}", todo!());
+    println!("part 2: {}", todo!());
+
+    Ok(())
+}
